c965f7baa3851cc12d84b0faff20e99081699533
Gerardy Lucas <gerardy.lucas.p@gmail.com>
2022-09-20 10:00:05 +0700

    py basic knowledge

---
+++ Main.py
@@ -0,0 +1,19 @@
+print("hello world!!!!")
+
+print("hey")
+print("friend")
+
+# lol
+
+"""
+    multiline
+    comment
+"""
+
+a = 10
+print(a)
+
+# python bisa di compile ke bytecode (seperti .exe)
+
+
+
