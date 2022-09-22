ebc98cd7089947385ef31a2e4179f7125dd984d6
Gerardy Lucas <gerardy.lucas.p@gmail.com>
2022-09-22 16:36:44 +0700

    learn python 5

---
+++ list.py
@@ -0,0 +1,149 @@
+
+
+data = [1, 2, 3, 4, 5]
+
+
+
+data_range = range(1, 11)
+
+change_to_list = list(data_range)
+print(change_to_list)
+
+
+list_data = []
+
+for i in range(1, 11):
+    list_data.append(i)
+
+
+print(list_data)
+
+
+
+
+# tambah data list
+
+data2 = ["budi", "dono", "siti", "bambang"]
+
+data2.insert(1, "yono") # data2.insert(posisi, item)
+print(data2)
+
+
+data2.append("gilang") # tambah data (mirip push)
+print(data2)
+
+
+# gabungkan data dari 2 list
+
+data_angka_baru = [5, 6, 7, 8, 9]
+
+data.extend(data_angka_baru)
+print(data)
+
+
+
+data2[3] = "danang"
+print(data2)
+
+
+# menghapus dari list
+
+data2.remove("budi")
+print(data2)
+
+
+
+data2.pop()
+print(data2)
+
+
+
+
+
+
+
+data3 = [3, 5, 1, 4, 9, 8, 7, 7, 2, 3]
+
+jumlah_data_7 = data3.count(7)
+print(jumlah_data_7)
+
+
+
+# urutkan data
+
+data3.sort()
+print(data3)
+
+
+
+# sort terbalik (reverse)
+
+data3.reverse()
+print(data3)
+
+
+
+# ambil posisi data
+
+data2 = ["budi", "dono", "siti", "bambang"]
+
+ambil_index_list = data2.index("siti")
+print(ambil_index_list)
+
+
+
+
+
+# duplicate list with copy
+
+data4 = data2.copy()
+
+data4[0] = "lala"
+
+print(data2)
+print(data4)
+
+
+
+
+# enumerate (can get index and the data value)
+
+data5 = [1, 4, 2, 64, 55, 89, 2]
+
+for i, data in enumerate(data5):
+    print(data)
+    
+
+
+
+
+list_book = []
+
+while True:
+    title = input("masukan judul buku\t: ")
+    writer = input("masukan nama penulis\t: ")
+    
+    new_book = [title, writer]
+    list_book.append(new_book)
+
+    i = 1
+    for book in list_book:
+        print(f"buku {i}")
+        print(f"nama buku\t: {book[0]}")
+        print(f"penulis\t\t: {book[1]}\n")
+        i += 1
+    
+    next = input("input next book? (y/n)\t: ")
+
+    if next == "y":
+        continue
+    else:
+        break
+    
+
+
+
+
+
+
+
--- looping.py
+++ looping.py
@@ -17,3 +17,42 @@ for i in range(1, 101):
         print(f"{i} FizzBuzz")
     else:
         print(i)
+
+
+
+i = 0
+while i < 10:
+    print(i)
+    i += 1
+
+
+
+i = 0
+while i < len(data):
+    print(data[i])
+    i += 1
+
+
+# break similar to return but implemented in loop
+i = 0
+cust = ""
+while i < len(data):
+    if (data[i] == "bejo"):
+        cust = data[i]
+        break
+    
+    cust = ""
+    i += 1
+
+print(cust)
+
+
+line = int(input("masukan panjang segitiga: "))
+i = 0
+
+stair = ""
+while i < line:
+    i += 1
+    stair = "*"*i
+
+    print(stair)
